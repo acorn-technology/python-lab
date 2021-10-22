aab5ffd8da52e74462e0837bf2801aa186862bea
Ken Alexandersson <ken.alexandersson@jeppesen.com>
2021-10-22 12:37:08 +0200

    lab 1.2

--- Lab1-IO/ken/ken-pi.py
+++ Lab1-IO/ken/ken-pi.py
@@ -1,6 +1,8 @@
 import math
 
-no_of_decimals = ".2f"
+user_input = input("How many decimals do you want me to use when showing Pi? ")
+
+no_of_decimals = "." + user_input + "f"
 pi_value = format(math.pi, no_of_decimals)
 
 print(f"Pi has the value {pi_value}")
