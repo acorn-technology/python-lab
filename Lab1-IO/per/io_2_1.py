66fb90990784e92f800386be0ecf271f2a473e01
Per Mattsson <per1978@gmail.com>
2021-10-24 18:37:13 +0200

    IO-2-1: Single file tail

--- Lab1-IO/per/io_2_1.py
+++ Lab1-IO/per/io_2_1.py
@@ -1,16 +1,16 @@
 import sys
 
 if __name__ == '__main__':
+    n_lines = 10
     file_name = sys.argv[1]
 
     buffer = []
-    n_lines = 10
     with open(file_name) as fp:
         while True:
             line = fp.readline()
             buffer.append(line)
 
-            if len(buffer) > 10:
+            if len(buffer) > n_lines:
                 buffer.pop(0)
 
             if not line:
